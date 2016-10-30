5a8bc5f6428a9b4e73502af0acca266d763230fe
FibonacciGirl <isdestefano@vassar.edu>
2016-10-30 19:05:40 -0400

    Defined variables
    Defined inputs
    And constants

--- GripTest.c
+++ GripTest.c
@@ -1,17 +1,42 @@
-int LMOTOR = 0;
-int RMOTOR = 3;
+
+//Define input channels for actuators and sensors
+const int LEFT_MOTOR = 0;
+const int RIGHT_MOTOR = 3;
+const int FRONT_BUMP = 15;
+const int BACK_BUMP = 14; 
+const int LEFT_PHOTO = 3;
+const int RIGHT_PHOTO = 4;
+const int LEFT_IR = 2;
+const int RIGHT_IR = 1;
+const int BACK_LEFT_PHOTO = 6;  
+const int BACK_RIGHT_PHOTO = 7; 
+
+//Define boolean constants
+const int TRUE = 1;
+const int FALSE = 0;
+
+//Speed in move function will be a percentage of top speed
+const int TOP_SPEED = 100;
+
 
 void forward(){
-	motor(LMOTOR, 50);
-	motor(RMOTOR, 50);
+	motor(L_MOTOR, .5*TOP_SPEED);
+	motor(R_MOTOR, .5*TOP_SPEED);
 	msleep(25);
 }
 
 void turnLeft(){
-	motor(LMOTOR, -75);
-	motor(RMOTOR, 75);
+	motor(L_MOTOR, -.75*TOP_SPEED);
+	motor(R_MOTOR, .75*TOP_SPEED);
 	msleep(100);
 }
+
+void turnRight() {
+	motor(R_MOTOR, -.75*TOP_SPEED);
+	motor(L_MOTOR,.75*TOP_SPEED);
+	msleep(100);
+}
+
 
 
 int main (){
